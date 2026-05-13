6a68a862de757e6cada54ded6642939af8cba7e9
alewnardu <lewnardu@gmail.com>
2026-05-13 17:55:57 -0300

    Implementa rota alterar senha (#5)
    * Adiciona dependencia de criptograria do mysql. Bloqueia retorno de senha no schema. Atualiza o requirements.
    
    * Implementa a rota de alteração de senha. Atualiza o arquivo postman.

--- app/services/autenticacao_service.py
+++ app/services/autenticacao_service.py
@@ -1,5 +1,8 @@
 from app.repositories.usuario_repository import UsuarioRepository
-from werkzeug.security import check_password_hash
+from werkzeug.security import (
+    check_password_hash,
+    generate_password_hash
+)
 
 class AutenticacaoService:
 
@@ -20,3 +23,26 @@ class AutenticacaoService:
             raise Exception('Senha inválida')
 
         return usuario
+    
+    def alterar_senha(usuario_id, dados):
+        
+        usuario = UsuarioRepository.buscar_por_id(usuario_id)
+
+        if not usuario:
+            raise Exception('Usuário não encontrado')
+        
+        senha_atual = dados.get('senha_atual')
+        nova_senha = dados.get('nova_senha')
+        confirmacao_senha = dados.get('confirmacao_senha')
+
+        if not all([senha_atual, nova_senha, confirmacao_senha]):
+            raise Exception('Informe a senha atual, nova senha e confirmação da nova senha')
+        
+        if not check_password_hash(usuario.senha, senha_atual):
+            raise Exception('Senha atual incorreta')
+
+        if nova_senha != confirmacao_senha:
+            raise Exception('A nova senha e a confirmação não coincidem')
+        
+        usuario.senha = generate_password_hash(nova_senha)
+        UsuarioRepository.salvar(usuario)
