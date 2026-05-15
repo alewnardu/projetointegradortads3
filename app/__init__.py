4c2e6d638b50a9f12cc8efff0daae9daec2b971f
alewnardu <lewnardu@gmail.com>
2026-05-15 18:56:06 -0300

    Implementa a recuperação de senha (#7)
    * Retira o alias para o exception generico do erro 500
    
    * Impementa metodo de recuperacao de senha
    
    * Implementa metodo de recuperacao de senha
    
    * Atualização do arquivo postman
    
    * Adiciona a dependencia de envio de email no core do projeto
    
    * Atualiza o arquivo de configuracoes para mapear do .env as configuracoes de email
    
    * Atualiza arquivo de exceptions
    
    * Adiciona a dependencia de envio de email no core do projeto
    
    * Implementa rota e servico de redefinição de senha
    
    * Implementa servico de envio de email
    
    * Atualiza arquivo de dependencias

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.extensions import db, migrate, jwt
+from app.extensions import db, migrate, jwt, mail
 from app.blueprints import lista_blueprints
 from app.config import Config
 
@@ -12,6 +12,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+    mail.init_app(app)
     
     for blueprint in lista_blueprints():
         app.register_blueprint(blueprint)
--- app/services/autenticacao_service.py
+++ app/services/autenticacao_service.py
@@ -1,8 +1,12 @@
+
+from flask import current_app
 from app.repositories.usuario_repository import UsuarioRepository
-from werkzeug.security import (
-    check_password_hash,
-    generate_password_hash
-)
+from werkzeug.security import (check_password_hash, generate_password_hash)
+from app.exceptions import *
+from datetime import timedelta
+from flask_jwt_extended import create_access_token, decode_token
+from app.services.email_service import EmailService
+from jwt import ExpiredSignatureError
 
 class AutenticacaoService:
 
@@ -11,25 +15,74 @@ class AutenticacaoService:
         email = dados.get('email')
         senha = dados.get('senha')
 
-        if not email or not senha:
+        if not all([email, senha]):
             raise ValidationError('Email e senha são obrigatórios')
         
         usuario = UsuarioRepository.buscar_por_email(email)
 
         if not usuario:
-            raise NotFoundError('Usuário não encontrado com o email informado')
+            raise NotFoundError('Credendiais inválidas')
         
         senha_valida = check_password_hash(usuario.senha, senha)
         if not senha_valida:
-            raise ValidationError('Senha inválida')
+            raise AuthenticationError('Falha na autenticação')
 
         return usuario
     
     @staticmethod
+    def recuperar_senha(dados):
+        email = dados.get('email')
+        if not email:
+            raise ValidationError('Email é obrigatório para recuperação de senha')
+        
+        usuario = UsuarioRepository.buscar_por_email(email)
+        if not usuario:
+            return True
+        
+        reset_token = create_access_token(identity=str(usuario.id), expires_delta=timedelta(minutes=5), additional_claims={'purpose': 'password_reset'})
+
+        frontend_url = current_app.config.get('LOCAL_APP_URL')
+        
+        reset_link = f"{frontend_url}/resetar-senha?token={reset_token}"
+
+        EmailService.enviar_email_recuperacao_senha(usuario, reset_link)
+        return True
+
+    @staticmethod
+    def redefinir_senha(dados):
+        
+        token = dados.get('token')
+        if not token:
+            raise AuthenticationError('Token inválido ou expirado 1')
+
+        nova_senha = dados.get('nova_senha')
+        confirmacao_senha = dados.get('confirmacao_senha')
+
+        if not all([nova_senha, confirmacao_senha]):
+            raise ValidationError('Os campos nova senha e confirmação de senha são obrigatórios 2')
+        
+        if nova_senha != confirmacao_senha:
+            raise ValidationError('A nova senha e a confirmação não coincidem 3')
+        
+        try:
+            decoded_token = decode_token(token)
+            if decoded_token.get('purpose') != 'password_reset':
+                raise AuthenticationError('Token inválido 4')
+            usuario_id = decoded_token.get('sub')
+        except Exception as e:
+            raise AuthenticationError(f'{str(e)} 5')
+
+        usuario = UsuarioRepository.buscar_por_id(usuario_id)
+        if not usuario:
+            raise ValidationError('Usuário não encontrado 6')
+
+        usuario.senha = generate_password_hash(nova_senha)
+        return UsuarioRepository.salvar(usuario)
+
+    @staticmethod
     def alterar_senha(usuario_id, dados):
         
         usuario = UsuarioRepository.buscar_por_id(usuario_id)
-
         if not usuario:
             raise NotFoundError('Usuário não encontrado')
         
@@ -38,10 +91,10 @@ class AutenticacaoService:
         confirmacao_senha = dados.get('confirmacao_senha')
 
         if not all([senha_atual, nova_senha, confirmacao_senha]):
-            raise ValidationError('Informe a senha atual, nova senha e confirmação da nova senha')
+            raise ValidationError('Os campos senha atual, nova senha e confirmação de senha são obrigatórios')
         
         if not check_password_hash(usuario.senha, senha_atual):
-            raise ValidationError('Senha atual incorreta')
+            raise AuthenticationError('Senha atual incorreta')
 
         if nova_senha != confirmacao_senha:
             raise ValidationError('A nova senha e a confirmação não coincidem')
