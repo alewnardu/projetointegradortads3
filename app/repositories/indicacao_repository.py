30f3e644ad9412bb61164c6151f85adb9ee1a477
alewnardu <lewnardu@gmail.com>
2026-05-16 13:16:02 -0300

    Implementa a busca da indicação (#12)
    * Ajusta o arquivo de dependências. Ajusta a rota e o serviço de listagem de indicações.
    
    * Implementa rota, servico e repositoy da busca de indicação. Atualiza o arquivo postman.

--- app/repositories/indicacao_repository.py
+++ app/repositories/indicacao_repository.py
@@ -4,5 +4,11 @@ from app.extensions import db
 class IndicacaoRepository:
     
     @staticmethod
-    def listar():
-        return Indicacao.query.all()
+    def listar(usuario_logado):
+        if not usuario_logado:
+            return Indicacao.query.all()
+        return Indicacao.query.filter_by(usuario_indicador_id=usuario_logado.id).all()
+    
+    @staticmethod
+    def buscar_por_id(indicacao_id):
+        return Indicacao.query.get(indicacao_id)
--- app/services/indicacao_service.py
+++ app/services/indicacao_service.py
@@ -1,6 +1,7 @@
 from app.models.indicacao import Indicacao
 from app.repositories.indicacao_repository import IndicacaoRepository
 from app.exceptions import *
+from app.repositories.usuario_repository import UsuarioRepository
 
 class IndicacaoService:
 
@@ -13,6 +14,27 @@ class IndicacaoService:
         usuario_logado = UsuarioRepository.buscar_por_id(usuario_logado_id)
         if not usuario_logado:
             raise ForbiddenError('Acesso negado! Você não tem permissão para acessar esta funcionalidade')
+        
+        if usuario_logado.perfil == 'ADMIN':
+            return IndicacaoRepository.listar(None)
 
-        indicacoes = IndicacaoRepository.listar_por_usuario(usuario_logado_id)
-        return indicacoes
+        return IndicacaoRepository.listar(usuario_logado)
+    
+    @staticmethod
+    def buscar_indicacao(indicacao_id, usuario_logado_id):
+
+        if not usuario_logado_id:
+            raise UnauthorizedError('Acesso negado! Esta funcionalidade requer autenticação')
+        
+        usuario_logado = UsuarioRepository.buscar_por_id(usuario_logado_id)
+        if not usuario_logado:
+            raise ForbiddenError('Acesso negado! Você não tem permissão para acessar esta funcionalidade')
+        
+        indicacao = IndicacaoRepository.buscar_por_id(indicacao_id)
+        if not indicacao:
+            raise NotFoundError('Indicação de brinquedoteca não encontrada')
+
+        if usuario_logado.perfil != 'ADMIN' and indicacao.usuario.id != usuario_logado.id:
+            raise ForbiddenError('Acesso negado! Você não tem permissão para acessar essa indicação.')
+
+        return indicacao
