2342a37a1065ba4c764d824b46dd7f8f6da170e2
alewnardu <lewnardu@gmail.com>
2026-05-16 00:27:38 -0300

    Implementa rota, servico e repository da listagem de indicacoes. Atualiza lista de blueprints. Cria migracao da tabela indicacao.

--- app/blueprints.py
+++ app/blueprints.py
@@ -1,6 +1,7 @@
 from app.routes.pagina_inicial import pagina_inicial_bp
 from app.routes.usuario_routes import usuario_bp
 from app.routes.autenticacao_routes import autenticacao_bp
+from app.routes.indicacao_routes import indicacao_bp
 
 def lista_blueprints():
-    return [pagina_inicial_bp, usuario_bp, autenticacao_bp]
+    return [pagina_inicial_bp, usuario_bp, autenticacao_bp, indicacao_bp]
